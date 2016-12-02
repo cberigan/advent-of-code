8fae5fdc0eeac7c4334159703d777d404ca28f3d
Corey Berigan <cberigan1@gmail.com>
2016-12-02 08:59:03 -0500

    modify resource loader

--- advent-of-code/src/main/java/com/aoc/helpers/Resources.java
+++ advent-of-code/src/main/java/com/aoc/helpers/Resources.java
@@ -2,13 +2,19 @@ package com.aoc.helpers;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Resources {
-
+	private static final boolean IS_WINDOWS = System.getProperty( "os.name" ).contains( "indow" );
+	
 	public static String LoadTextFile(String path) throws IOException{
 		//Get file from resources folder
 		ClassLoader classLoader = Resources.class.getClassLoader();
-		return new String(Files.readAllBytes(Paths.get(classLoader.getResource(path).getPath())));
+		String absPath = classLoader.getResource(path).getPath();
+		String osAppropriatePath = IS_WINDOWS ? absPath.substring(1) : absPath.toString();
+		Path newPath = Paths.get(osAppropriatePath);
+		
+		return new String(Files.readAllBytes(newPath));
 	}
 }

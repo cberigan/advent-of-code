ec2887e1e975cce1950f8d234033b3343e83542c
Corey Berigan <cberigan@novetta.com>
2016-12-09 02:11:03 -0500

    problem 8 good state part 1

---
+++ AdventOfCode/AOC.Problem9/Op.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC.Problem9
+{
+    class Op
+    {
+        public int Chars { get; set; }
+        public int Repeat { get; set; }
+
+        public Op(int c, int r)
+        {
+            Chars = c;
+            Repeat = r;
+        }
+    }
+}
---
+++ AdventOfCode/AOC.Problem9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC.Problem9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string raw = File.ReadAllText("data.txt");
+            string result = "";
+
+            for (int i = 0; i < raw.Length; i++)
+            {
+                if (raw[i].Equals('(')) {
+                    string uncompressed = "";
+                    i = ProcessCompresion(raw,i, out uncompressed);
+                    result += uncompressed;
+                }
+                else
+                {
+                    result += raw[i];
+                }
+            }
+
+            Console.WriteLine(result.Length);
+            Console.WriteLine("ABBBBBC"); 
+            Console.ReadLine();
+        }
+
+        private static int ProcessCompresion(string raw, int i, out string result)
+        {
+            var paran = raw.IndexOf(')', i) - 1;
+            var sub = raw.Substring(i + 1, paran - i);
+            var p = sub.Split('x');
+            var numChars = int.Parse(p[0]);
+            var repeat = int.Parse(p[1]);
+            
+
+            var value = raw.Substring(paran + 2, numChars);
+            result = string.Concat(Enumerable.Repeat(value, repeat));
+            return paran + 1 + value.Length;
+        }
+    }
+}

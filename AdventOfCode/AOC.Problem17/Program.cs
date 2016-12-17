5fffe378088cffc2bace2555f99717c224d16690
Corey Berigan <cberigan@novetta.com>
2016-12-17 01:08:01 -0500

    day 17 done

---
+++ AdventOfCode/AOC.Problem17/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC.Problem17
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(PartOne().GetPath());
+            Console.WriteLine(PartTwo());
+            Console.ReadLine();
+        }
+
+        static Room PartOne()
+        {
+            HashSet<Room> explored = new HashSet<Room>();
+            Queue<Room> toExplore = new Queue<Room>();
+
+            toExplore.Enqueue(new Room(0, 0, "veumntbg"));
+
+            while (true)
+            {
+                if (toExplore.Count == 0) return null;
+                else
+                {
+                    Room currentState = toExplore.Dequeue();
+                    explored.Add(currentState);
+                    var nextStates = currentState.GenerateNextStates();
+                    foreach (var s in nextStates)
+                    {
+                        if (!explored.Contains(s) && !toExplore.Contains(s))
+                        {
+                            if (s.IsGoal()) return s;
+                            else toExplore.Enqueue(s);
+                        }
+                    }
+                }
+            }
+        }
+
+        static int PartTwo()
+        {
+            List<Room> rooms = new List<Room>();
+            HashSet<Room> explored = new HashSet<Room>();
+            Queue<Room> toExplore = new Queue<Room>();
+
+            toExplore.Enqueue(new Room(0, 0, "veumntbg"));
+
+            while (true)
+            {
+                if (toExplore.Count == 0) break;
+                else
+                {
+                    Room currentState = toExplore.Dequeue();
+                    explored.Add(currentState);
+                    var nextStates = currentState.GenerateNextStates();
+                    foreach (var s in nextStates)
+                    {
+                        if (!explored.Contains(s) && !toExplore.Contains(s))
+                        {
+                            if (s.IsGoal()) rooms.Add(s);
+                            else toExplore.Enqueue(s);
+                        }
+                    }
+                }
+            }
+            return rooms.Select(r => r.GetPath().Length).Max();
+        }
+    }
+}
---
+++ AdventOfCode/AOC.Problem17/Room.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC.Problem17
+{
+    public class Room
+    {
+        //grid from (0,0),(3,0),(3,-3),(0,-3)
+        private int min_x = 0;
+        private int max_x = 3;
+        private int min_y = -3;
+        private int max_y = 0;
+        private static MD5 md5 = MD5.Create();
+        static HashSet<char> open = new HashSet<char> { 'b', 'c', 'd', 'e', 'f' };
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public string HashState { get; private set; }
+
+        public Room(int x, int y, string hash)
+        {
+            X = x;
+            Y = y;
+            HashState = hash;
+        }
+
+        public List<Room> GenerateNextStates()
+        {
+            List<Room> rooms = new List<Room>();
+            var hash = GetHash(HashState);
+            if (open.Contains(hash[0]) && Y + 1 <= max_y) rooms.Add(new Room(X, Y + 1, HashState + "U"));
+            if (open.Contains(hash[1]) && Y - 1 >= min_y) rooms.Add(new Room(X, Y - 1, HashState + "D"));
+            if (open.Contains(hash[2]) && X - 1 >= min_x ) rooms.Add(new Room(X - 1, Y, HashState + "L"));
+            if (open.Contains(hash[3]) && X + 1 <= max_x) rooms.Add(new Room(X + 1, Y, HashState + "R"));
+            return rooms;
+        }
+
+        internal bool IsGoal()
+        {
+            return X == 3 && Y == -3;
+        }
+
+        public string GetPath()
+        {
+            return HashState.Remove(0, 8);
+        }
+
+        private string GetHash(string val)
+        {
+            var temp = md5.ComputeHash(Encoding.ASCII.GetBytes(val));
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < temp.Length; j++)
+            {
+                sb.Append(temp[j].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}

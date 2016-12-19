ec34a4e52883a38f800960a2cdc76862112b291c
Corey Berigan <cberigan1@gmail.com>
2016-12-19 11:05:06 -0500

    code cleanup

---
+++ AdventOfCode/AOC.Problem19/Elf.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC.Problem19
+{
+    public class Elf
+    {
+        private static Elf elfTracking = null;
+        private static int elfs = 0;
+        public int Num { get; private set; }
+        public int Presents { get; set; }
+
+        internal static void SetElfNumber(int numElfs)
+        {
+            elfs = numElfs;
+        }
+
+        public Elf Left { get; set; }
+        public Elf Right { get; set; }
+
+        public Elf(int num, int presents)
+        {
+            Num = num;
+            Presents = presents;
+        }
+
+        public void TakePresentsLeft()
+        {
+            this.Presents += Left.Presents;
+            Left.RemoveElf();
+        }
+
+        public static void SetElfTracking(Elf elf)
+        {
+            elfTracking = elf;
+        }
+
+        public void RemoveElf()
+        {
+            this.Right.Left = this.Left;
+            this.Left.Right = this.Right;
+        }
+
+        public void TakePresentsAcross()
+        {
+            this.Presents += elfTracking.Presents;
+            //link both left and right of current
+            elfTracking.RemoveElf();
+            elfTracking = elfs % 2 == 1 ? elfTracking.Left.Left : elfTracking.Left;
+            elfs--;
+        }
+    }
+}
--- AdventOfCode/AOC.Problem19/Program.cs
+++ AdventOfCode/AOC.Problem19/Program.cs
@@ -8,104 +8,58 @@ namespace AOC.Problem19
 {
     class Program
     {
+        static int numElfs = 3005290;
         static void Main(string[] args)
         {
-            int numElfs = 3005290;
-            
+            Part1();
+            Part2();
+            Console.ReadLine();
+        }
 
-            List<Elf> elfs = new List<Elf>();
+        static void Part1()
+        {
+            Elf current = BuildCircle();
+            while (current.Presents != numElfs)
+            {
+                current.TakePresentsLeft();
+                current = current.Left;
+            }
+            Console.WriteLine("Part 1: Elf " + current.Num + " has all the presents.");
+        }
 
+        static void Part2()
+        {
+            Elf.SetElfNumber(numElfs);
+            Elf current = BuildCircle();
+            while (current.Presents != numElfs)
+            {
+                current.TakePresentsAcross();
+                current = current.Left;
+            }
+            Console.WriteLine("Part 2: Elf " + current.Num + " has all the presents.");
+        }
+
+        static Elf BuildCircle()
+        {
             Elf first = new Elf(1, 1);
             Elf current = first;
             for (int i = 1; i < numElfs; i++)
             {
-                current.Left = new Elf(i+1, 1);
+                var newElf = new Elf(i + 1, 1);
+                current.Left = newElf;
                 var before = current;
                 current = current.Left;
                 current.Right = before;
+                if (i == numElfs / 2) Elf.SetElfTracking(newElf);
                 if (i == numElfs - 1)
                 {
                     current.Left = first;
                     first.Right = current;
                 }
             }
-            current = first;
-            while(current.Presents != numElfs)
-            {
-                current.TakePresentsAcross();
-                current = current.Left;
-            }
-            Console.WriteLine(current.Num);
-            Console.ReadLine();
-        }
-
-        private static int mod(int x, int m)
-        {
-            return (x % m + m) % m;
+            return first;
         }
     }
 
-    public class Elf
-    {
-        public static Elf elfTracking = null;
-        private static int elfs = 3005290;
-        public int Num { get; private set; }
-        public int Presents { get; set; }
-        public Elf Left { get; set; }
-        public Elf Right { get; set; }
-
-        public Elf(int num, int presents)
-        {
-            Num = num;
-            Presents = presents;
-        }
-
-        public void TakePresentsLeft()
-        {
-            this.Presents += Left.Presents;
-            //relink elf to the left of the next
-            this.Left = Left.Left;
-        }
-
-        public decimal GetDegree()
-        {
-            return (decimal)360.0 / elfs;
-        }
-
-        public void TakePresentsAcross()
-        {
-            if (elfTracking == null)
-            {
-                int count = 0;
-                Elf current = this;
-                while ((decimal)count * GetDegree() <= 180.0m)
-                {
-                    var d2 = count * GetDegree();
-                    count++;
-                    current = current.Left;
-                }
-                elfTracking = current.Right;
-            }
-            this.Presents += elfTracking.Presents;
-            //link both left and right of current
-            elfTracking.Right.Left = elfTracking.Left;
-            elfTracking.Left.Right = elfTracking.Right;
-
-            elfTracking = elfs % 2 == 1 ? elfTracking.Left.Left : elfTracking.Left;
-            
-           
-            elfs--;
-        }
-
-        public override bool Equals(object obj)
-        {
-            var other = obj as Elf;
-            if (other == null) return false;
-            return other.Num == this.Num;
-        }
-        public override int GetHashCode()
-        {
-            return Num.GetHashCode();
-        }
-    }
+    
 }
